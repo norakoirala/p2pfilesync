274a939b0afba9878fc333a247aae35a8ea0037e
norakoirala <norakoirala@localhost>
2020-05-02 12:38:49 -0700

    comments added, bool detected changed to fileChange

--- ListenerThread.java
+++ ListenerThread.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package peertopeer;
 
 import java.net.ServerSocket;
@@ -12,44 +7,54 @@ import java.util.ArrayList;
 /**
  *
  * @author RJ
+ * Listener thread scans for new connections
  */
 public class ListenerThread extends Thread {
-     
-    ArrayList<Socket> connections;
-    ServerSocket server;
-    boolean detected = false;
-   ListenerThread () {
-      // The compiler creates the byte code equivalent of super ();
-   }
- 
-   public ListenerThread (ArrayList<Socket> c, ServerSocket s) {
-      // The compiler creates the byte code equivalent of super ();
-    this.server =s;
-     this.connections = c;
-   }
-   
-   
 
-   
-   public void run () {
+	ArrayList<Socket> connections; //keeps a list of all the connections on the network
+	ServerSocket server; //the node that is the server
+	boolean fileChange = false; //boolean to check if any file changes have occured
+    
+	/**
+	 * Default Constructor 
+	 */
+	ListenerThread () {
+      // The compiler creates the byte code equivalent of super ();
+	}
+	
+	/**
+	 * Overloaded constructor
+	 * @param c - the list of connections already on the network
+	 * @param s - the server node 
+	 */
+	public ListenerThread (ArrayList<Socket> c, ServerSocket s) {
+      // The compiler creates the byte code equivalent of super ();
+		this.server =s;
+		this.connections = c;
+	}
+	
+	/*
+	 * Method runs to scan the network for new connections 
+	 */
+	public void run () {
        try{
-          
-           while(!detected){
-            Socket tmp;
-            connections.add(tmp = server.accept());
-            System.out.println("Added  new connection: " + tmp);
-           
-         
+    	   
+          //while there are no files changes, accept new connections 
+           while(!fileChange){
+            Socket tmp; 
+            connections.add(tmp = server.accept()); //adds new connection to list of connections 
+            System.out.println("Added  new connection: " + tmp); //verifying new connection
            }
-           
            
        } catch ( Exception e) {
            
        }
-   }
-   
-   public void kill(){
-   
-       this.detected = true;
-   }
+	}
+	
+	/*
+	 * Method switches boolean to stop scanning the network because a file was addded, removed, updated
+	 */
+	public void kill(){
+       this.fileChange = true;
+	}
 }
