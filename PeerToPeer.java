45499c8e33615551d327a86337a6d2a83b049353
norakoirala <norakoirala@localhost>
2020-05-02 13:41:39 -0700

    Comments added

--- PeerToPeer.java
+++ PeerToPeer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package peertopeer;
 
 import java.math.BigInteger;
@@ -11,40 +6,40 @@ import java.security.MessageDigest;
 /**
  *
  * @author RJ
+ * @author Nora
+ * PeerToPeer runs the main method, has hashing function
  */
 public class PeerToPeer {
 
     /**
+     * Main Method to run program
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
 		// TODO code application logic here
 		     
 		sNode n1 = new sNode(1);
-		new FolderThread(n1).start();
-		// new ReceiverThread(n1).start(); 
+		//new FolderThread(n1).start();
+		 new ReceiverThread(n1).start(); 
     }
     
-    
-    
-    
+    /**
+     * Method takes in input and returns the encrypted message
+     * @param input - the string to be hashed using SHA-1
+     * @return - the hashed string 
+     * @throws Exception
+     */
     public static String encrypt(String input) throws Exception {
-    
-        MessageDigest md = MessageDigest.getInstance("SHA-1"); 
-        byte[] messageDigest = md.digest(input.getBytes());
-        BigInteger no = new BigInteger(1, messageDigest); 
-        String hashtext = no.toString(16); 
+        MessageDigest md = MessageDigest.getInstance("SHA-1"); //implementing SHA-1
+        byte[] messageDigest = md.digest(input.getBytes()); //using SHA-1 to convert message to bytes
+        BigInteger no = new BigInteger(1, messageDigest); //converting byes into bigInteger
+        String hashtext = no.toString(16); //converting bigInteger to String
         
         while (hashtext.length() < 32) { 
-            hashtext = "0" + hashtext; 
+            hashtext = "0" + hashtext; //converting to 32 bit message
         }
         
         return hashtext; 
-       
     }
     
-    
 }
-    
-    
-
