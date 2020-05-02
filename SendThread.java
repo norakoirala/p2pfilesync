cf40646a0bb8561b20ed10cc3bf5eed171a855ee
norakoirala <norakoirala@localhost>
2020-05-02 16:52:27 -0700

    Added comments

--- SendThread.java
+++ SendThread.java
@@ -1,51 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package peertopeer;
 
 import java.net.Socket;
 import java.util.ArrayList;
 
 /**
- *
+ * Send Thread is the thread that send runs on 
  * @author RJ
+ * @author Nora
  */
  public class SendThread extends Thread {
-    Socket socket;
-    String fileName;
-    sNode s;
-    ArrayList<Socket> connections;
-   SendThread () {
+	String fileName; //the file being sent
+	sNode s; 
+	ArrayList<Socket> connections; //the list of all the connections on the network
+	
+	/**
+	 * Default constructor
+	 */
+	SendThread () {
       // The compiler creates the byte code equivalent of super ();
-   }
-   SendThread (String name) {
+	}
+	
+	/**
+	 * Overloaded constructor
+	 * @param name
+	 */
+	SendThread (String name) {
       super (name); // Pass name to Thread superclass
-   }
+	}
    
-   public SendThread (ArrayList<Socket> cn, String st ,sNode sn) {
-      // The compiler creates the byte code equivalent of super ();
-  
-      this.fileName = st;
-      this.s = sn;
-      this.connections = cn;
-   }
-   
-   
-
-   
-   public void run () {
-       try{
-            System.out.println("Thread");
-            for(Socket s : connections){
-                this.s.outgoing.send(s,fileName); 
-                System.out.println("Sending to... " +  s);
-            }
+	/**
+	 * Overloaded constructor
+	 * @param cn - the list of connections 
+	 * @param st - the file name 
+	 * @param sn
+	 */
+	public SendThread (ArrayList<Socket> cn, String st ,sNode sn) {
+		// The compiler creates the byte code equivalent of super ();
+		this.fileName = st;
+		this.s = sn;
+		this.connections = cn;
+	}
+	/**
+	 * Method runs the thread to send files
+	 */
+	public void run () {
+		try{
+			for(Socket s : connections){ //sends file to connections
+				this.s.outgoing.send(s,fileName); 
+				System.out.println("Sending to... " +  s);
+			}
        } catch ( Exception e) {
-           
+           e.printStackTrace();
        }
    }
-    
-  
+	
 }
