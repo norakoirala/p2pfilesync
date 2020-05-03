a2f18a22bc2d7305041b446ba5b2fe1f209d8231
norakoirala <norakoirala@localhost>
2020-05-02 17:01:49 -0700

    Added comments

--- sNode.java
+++ sNode.java
@@ -1,64 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package peertopeer;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardWatchEventKinds;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
-import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
 import java.util.HashMap;
 
-
-
 /**
- *
+ * sNode is representation of a node
  * @author RJ
+ * @author Nora
  */
 public class sNode {
-    HashMap<String,sNode> fingerTable;
-    Sender outgoing;
+	
+    Sender outgoing; 
     Receiver incoming;
     int nodeNum;
     
-    
+    /**
+     * Overloaded constructor
+     * @param n - number for the node
+     */
     public sNode(int n){
         this.nodeNum = n;
     }
     
+    /**
+     * Method sends files
+     * @throws Exception
+     */
     public void listen() throws Exception {
         outgoing = new Sender(this);
         outgoing.watch(outgoing.sender());
     }
     
+    /**
+     * Method receives files
+     * @throws Exception
+     */
     public void receive() throws Exception{
         incoming = new Receiver();
         Socket cl = incoming.client();
         System.out.println("Broken?");
         incoming.acceptFile(cl,"s.pdf");
-        
     }
-
+    
 }
