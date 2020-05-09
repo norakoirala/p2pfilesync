a86b67010f9ccaf8fcee85e2ccbe39d11e7117c2
Nora <norakoirala@gmail.com>
2020-05-09 12:59:25 -0700

    Update ReceiverThread.java

--- ReceiverThread.java
+++ ReceiverThread.java
@@ -108,10 +108,10 @@ public class ReceiverThread extends Thread {
     		 try{                  
                 if(bootstrap != null ) {
                 	try{
-                    	System.out.println("Bootstrap Node: " + bootstrap.toString());
                     	BufferedInputStream bis = new BufferedInputStream(bootstrap.getInputStream());
                     	DataInputStream dis = new DataInputStream(bis);
                     	String check = dis.readUTF();
+                    	System.out.println(check);
                     	if (check.equals("IP")) { //receives IP from bootstrap 
     						String ip = dis.readUTF();    
     						System.out.println("New connection obtained " + ip.substring(1,ip.length()));
@@ -121,10 +121,11 @@ public class ReceiverThread extends Thread {
                         	//initialize components
                 			int files = dis.readInt();
                         	long time = dis.readLong();
+                        	long length = dis.readLong();
                         	 
                             //Displays file name
                             String fileName = dis.readUTF();
-                            System.out.println("Recieving " + files + "File: " + fileName);
+                            System.out.println("Recieving " + files + "File: " + fileName + " From: " + bootstrap);
                             
                             //Makes directory + creates file
                             File dir = new File("FileDrop");
@@ -137,14 +138,12 @@ public class ReceiverThread extends Thread {
                             } 
                             
                             //Receives file + confirmation 
-                        	int bufferSize = (int) dis.readLong();
                             OutputStream output = new FileOutputStream("FileDrop/" + fileName);
-                            byte[] buffer = new byte[bufferSize];
-                            int read;
-                            
-                            while((read = dis.read(buffer)) != 1){
-                                output.write(buffer, 0, read);
-                            }
+                            int read = 0;
+    	                    while(read < length) {
+    	                        output.write(bis.read());
+    	                        read++;
+    	                    }
                             
                             output.flush();
                             System.out.println("Received File: " + fileName);
@@ -156,39 +155,41 @@ public class ReceiverThread extends Thread {
                 }
                 
                 for(Socket a : incoming) {
-                	System.out.println("Confirmed connection non bootstrap "  + a.toString());
                 	try {        	
                     	//initialize components
-                    	System.out.println("incoming from: " + a);
-                        int bufferSize = a.getReceiveBufferSize(); 
-                        InputStream in = a.getInputStream();
-                        DataInputStream clientData = new DataInputStream(in);
-                        String check = clientData.readUTF();
+                    
+                        BufferedInputStream bis = new BufferedInputStream(a.getInputStream());
+                        DataInputStream dis = new DataInputStream(bis);
+                        String check = dis.readUTF();
 
                         if(check.equals("File")) { //receives file from other nodes
-                        	long time = clientData.readLong();
+                        	//initialize components
+                			int files = dis.readInt();
+                        	long time = dis.readLong();
+                        	long length = dis.readLong();
+                        	 
                             //Displays file name
-                            String fileName = clientData.readUTF();
-                            System.out.println("Recieving File: " + fileName);
+                            String fileName = dis.readUTF();
+                            System.out.println("Recieving " + files + "File: " + fileName + " From: " + a);
                             
                             //Makes directory + creates file
                             File dir = new File("FileDrop");
                             if(!dir.exists()) dir.mkdir();
                             File tmp = new File("FileDrop/" + fileName);
                             
-                           
                             //Handles duplicate files 
                             if(tmp.exists() && (tmp.lastModified() <= time)) {
-                    			System.out.println("Updating file: " + fileName);
+                    			System.out.println("Updating file " + fileName);
                             } 
                             
                             //Receives file + confirmation 
-                            OutputStream output = new FileOutputStream("File Drop/" + fileName);
-                            byte[] buffer = new byte[bufferSize];
-                            int read;
-                            while((read = clientData.read(buffer)) != 1){
-                                output.write(buffer, 0, read);
-                            }
+                            OutputStream output = new FileOutputStream("FileDrop/" + fileName);
+                            int read = 0;
+    	                    while(read < length) {
+    	                        output.write(bis.read());
+    	                        read++;
+    	                    }
+                            
                             output.flush();
                             System.out.println("Received File: " + fileName);
                             tmp.setLastModified(time);
